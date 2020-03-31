505030d3ea4f2384150bcf3d12da504a8ff3bd7a
Michelle Frattaroli <mishyjari@outlook.com>
2020-03-31 18:39:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,5 +3,16 @@ class Dog
     @name = name
     @breed = breed
   end
-  attr_accessor :name, :breed
+  def name=(name)
+    @name = name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end
 end
